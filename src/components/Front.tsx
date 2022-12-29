4475666d394d1ac060db2a0b858192c1f352203c
Oya-Tomo <oyatomo.dev@gmail.com>
2022-12-29 19:31:26 +0900

    update: added support for Raku-Raku phone -beta

--- src/components/Front.tsx
+++ src/components/Front.tsx
@@ -32,44 +32,40 @@ const Front = () => {
     });
     return (
         <div className={style["base"]}>
-            <div className={style["message"]}>
-                <section className={style["first"]}>
-                    <h1>謹賀新年</h1>
-                </section>
-                <section className={style["middle"]}>
-                    <div className={style["contents"]}>
-                        <h1>新年明けましておめでとうございます。</h1>
+            <section className={style["first"]}>
+                <h1>謹賀新年</h1>
+            </section>
+            <section className={style["middle"]}>
+                <div className={style["contents"]}>
+                    <h1>新年明けましておめでとうございます。</h1>
+                    <p>
+                        旧年中はお世話になり有難うございました。また本年もよろしくお願い申し上げます。
+                    </p>
+                    <p>
+                        新型コロナの影響で色々と大変な世の中ですが、体調に気をつけて今年も一年頑張りましょう。
+                    </p>
+                    <div className={style["desc"]}>
+                        <h1>このページについて</h1>
                         <p>
-                            旧年中はお世話になり有難うございました。また本年もよろしくお願い申し上げます。
+                            このサイトはスマホで見ていただけると嬉しいです。
+                            ちなみに画像はすべて手作りです！
+                            ※PCでも見れるように作成してありますがデザインが崩れることがあります。
                         </p>
-                        <p>
-                            新型コロナの影響で色々と大変な世の中ですが、体調に気をつけて今年も一年頑張りましょう。
-                        </p>
-                        <div className={style["desc"]}>
-                            <h1>このページについて</h1>
-                            <p>
-                                このサイトはスマホで見ていただけると嬉しいです。
-                                ちなみに画像はすべて手作りです！
-                                ※PCでも見れるように作成してありますがデザインが崩れることがあります。
-                            </p>
-                        </div>
-                        <div className={style["sun_area"]}>
-                            <div
-                                className={
-                                    style[
-                                        sunRise ? "sun_visible" : "sun_hidden"
-                                    ]
-                                }
-                            ></div>
-                        </div>
-                        <img
-                            className={style["mountain"]}
-                            src={mountainImage}
-                            alt="mountain"
-                        />
                     </div>
-                </section>
-            </div>
+                    <div className={style["sun_area"]}>
+                        <div
+                            className={
+                                style[sunRise ? "sun_visible" : "sun_hidden"]
+                            }
+                        ></div>
+                    </div>
+                    <img
+                        className={style["mountain"]}
+                        src={mountainImage}
+                        alt="mountain"
+                    />
+                </div>
+            </section>
             <div className={style["ending"]}>
                 <div className={style["contents"]}>
                     <img
